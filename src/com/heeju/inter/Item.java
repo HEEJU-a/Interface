cea2bd6072639b2f1c7b287c95adba60bac7942c
HEEJU-a <heeju9923@naver.com>
2021-07-05 20:19:09 +0900

    Initial commit

---
+++ src/com/heeju/inter/Item.java
@@ -0,0 +1,95 @@
+package com.heeju.inter;
+
+
+public class Item {
+    private String dealPrice;
+    private int buildYear;
+    private int dealYear;
+    private String dong;
+    private String apartNm;
+    private int dealMon;
+    private int dealDay;
+    private double squre;
+    private int zipCode;
+    private int floor;
+
+    public String getDealPrice() {
+        return dealPrice;
+    }
+
+    public void setDealPrice(String dealPrice) {
+        this.dealPrice = dealPrice;
+    }
+
+    public int getBuildYear() {
+        return buildYear;
+    }
+
+    public void setBuildYear(int buildYear) {
+        this.buildYear = buildYear;
+    }
+
+    public int getDealYear() {
+        return dealYear;
+    }
+
+    public void setDealYear(int dealYear) {
+        this.dealYear = dealYear;
+    }
+
+    public String getDong() {
+        return dong;
+    }
+
+    public void setDong(String dong) {
+        this.dong = dong;
+    }
+
+    public String getApartNm() {
+        return apartNm;
+    }
+
+    public void setApartNm(String apartNm) {
+        this.apartNm = apartNm;
+    }
+
+    public int getDealMon() {
+        return dealMon;
+    }
+
+    public void setDealMon(int dealMon) {
+        this.dealMon = dealMon;
+    }
+
+    public int getDealDay() {
+        return dealDay;
+    }
+
+    public void setDealDay(int dealDay) {
+        this.dealDay = dealDay;
+    }
+
+    public double getSqure() {
+        return squre;
+    }
+
+    public void setSqure(double squre) {
+        this.squre = squre;
+    }
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public int getFloor() {
+        return floor;
+    }
+
+    public void setFloor(int floor) {
+        this.floor = floor;
+    }
+}
